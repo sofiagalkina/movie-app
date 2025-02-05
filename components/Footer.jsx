71900d92acac3eb62dfb6e85205044ff63962d64
sofiagalkina <gal18024@byui.edu>
2025-02-04 21:59:02 -0700

    started a new movie project

---
+++ components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+const Footer = () => {
+    return (
+        <footer>
+            <div>
+                <span> Created by </span>
+                <a href="#" target="_blank"
+                rel="noopener noreferrer"
+                >
+                    Sofia 
+                </a>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
