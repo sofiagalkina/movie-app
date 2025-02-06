a57ad09e1b88c1dd61b12f89dfa33d8e09e4dfeb
sofiagalkina <gal18024@byui.edu>
2025-02-05 21:26:55 -0700

    -

--- src/app/movies/page.js
+++ src/app/movies/page.js
@@ -1,9 +1,13 @@
+import Footer from "../../../components/Footer"
 export default function MoviesPage() {
     return(
         <div>
         <h1>Hello</h1>
         <p> if you see this, routing worked (no way)</p>
+
+        <Footer />
         </div>
+    
     
     )
       
