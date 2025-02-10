c85fcea46968998e9520ea8e6de9ae6953f753cd
sofiagalkina <gal18024@byui.edu>
2025-02-09 23:56:23 -0700

    search

--- src/app/movies/page.js
+++ src/app/movies/page.js
@@ -1,10 +1,17 @@
 import Footer from "../../../components/Footer"
+import Image
+ from "next/image"
 export default function MoviesPage() {
     return(
         <div>
         <h1>Hello</h1>
         <p> if you see this, routing worked (no way)</p>
-
+        <button>
+            <image src={refresh} alt="magnifying glass"></image>
+        </button>
+        <div>
+            Placeholder for movies
+        </div>
         <Footer />
         </div>
     
