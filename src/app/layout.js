4544a2b225756d2f62907d0da73756a8637cf1a3
sofiagalkina <gal18024@byui.edu>
2025-03-03 16:27:42 -0700

    -

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "../../components/Footer";
 import styles from "../../styles/Home.module.css";
-import axios from 'axios';
 
 
 const geistSans = Geist({
@@ -21,31 +20,7 @@ export const metadata = {
 };
 
 
-async function fetchData() {
-  
-   const options = {
-    url: 'https://imdb236.p.rapidapi.com/imdb/top250-movies',
-    method: "GET",
-    headers: {
-      'x-rapidapi-key': '0cea453a33msh4502e8811584130p136ef0jsnf29f9bfdebb6',
-      'x-rapidapi-host': 'imdb236.p.rapidapi.com'
-    }
-  }
-
-  try {
-    const response = await axios.request(options);
-    console.log(response.data);
-  } catch (error) {
-    console.error(error);
-  }
-
-}
-
-
 export default async function RootLayout({ children }) {
-
-  const data = await fetchData();
-  console.log(data)
 
   return (
     <html lang="en">
--- src/app/movies/page.js
+++ src/app/movies/page.js
@@ -1,18 +1,52 @@
-import Footer from "../../../components/Footer"
-import Image
- from "next/image"
-export default function MoviesPage() {
+import Footer from "../../../components/Footer";
+import Image from "next/image";
+import axios from "axios";
+import { headers } from "next/headers";
+
+async function fetchMovies() {
+    
+    const options = {
+        method: 'GET',
+        url: 'https://imdb236.p.rapidapi.com/imdb/top250-movies',
+        headers: {
+             'x-rapidapi-key': '0cea453a33msh4502e8811584130p136ef0jsnf29f9bfdebb6',
+             'x-rapidapi-host': 'imdb236.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await axios.request(options);
+        console.log(response.data);
+        return response.data;
+    } catch (error){
+        console.error(error);
+        return [];
+    }
+}
+
+export default async function MoviesPage() {
+
+    const movies = await fetchMovies();
+    console.log(`Type of movies: ${typeof movies}`)
+
     return(
-        <div>
-        <h1>Hello</h1>
-        <p> if you see this, routing worked (no way)</p>
-        <button>
-         hehe button
-        </button>
-        <div>
-            Placeholder for movies
-        </div>
-        <Footer />
+        <div className="movie-page">
+
+
+        <h1>Top 250 movies from the API:</h1>
+        {movies ? (
+            <ul>
+                {movies.map((movie) => (
+                    <li key={movie.id}>{movie.primaryTitle}</li>
+                ))}
+            </ul>
+        ) : (
+            <p>Failed to fetch movies from the API</p>
+        )
+
+        }
+
+     
         </div>
     
     
